15357dfe8dffae5816debf4c9a7e0347806d6186
lang999 <39425761+lang999@users.noreply.github.com>
2025-06-06 22:16:05 +0800

    Update 茄子.js

--- rules/茄子.js
+++ rules/茄子.js
@@ -1,7 +1,7 @@
 const csdown = {
     d: [],
     author: '流苏',
-    version: '20250516',
+    version: '20250606',
     rely: (data) => {
         return data.match(/\{([\s\S]*)\}/)[0].replace(/\{([\s\S]*)\}/, '$1')
     },
